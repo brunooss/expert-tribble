b61cf4dc8d803878cef0cc120503e0e03b79525a
brunooss <bruno.oss@outlook.com>
2018-08-28 23:49:46 -0300

    projeto: deletar arquivos css 'App.css' e 'index.css'

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,15 +1,11 @@
 import * as React from "react";
-import "./App.css";
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">
-            Welcome to/ Bienvenido a/ Bem vindo ao Expert Tribble
-          </h1>
-          <button formTarget="youtube.com">Press</button>
+          <h1 className="App-title">Expert Tribble</h1>
         </header>
       </div>
     );
