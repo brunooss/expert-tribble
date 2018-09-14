a1784e4b51bba2da2b921dcbbc426748d12e3b16
brunooss <bruno.oss@outlook.com>
2018-09-14 15:20:12 -0300

    projeto: alterar UI da tela inicial e adicionar biblioteca de funcionamento do Firebase Authentication

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,18 +1,42 @@
 import * as React from "react";
 import "./App.css";
+import "./style.css";
+import * as firebase from "firebase";
 
 class App extends React.Component {
   public render() {
+    firebase
+      .database()
+      .ref("message")
+      .set("Hello");
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">
-            Welcome to/ Bienvenido a/ Bem vindo ao Expert Tribble
-          </h1>
+        <header className="Header">
+          <h1 className="Title">Expert tribble</h1>
+          <div>
+            <i className="material-icons Icon">email</i>
+            <i className="material-icons Icon">stay_current_portrait</i>
+            <i className="material-icons Icon">chat</i>
+          </div>
         </header>
+        <form className="Login">
+          <h1>Log in</h1>
+          <input className="LoginInput" />
+          <br />
+          <input className="LoginInput" />
+        </form>
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+
+cd .\react-project\
+npm run build
+cd ..
+firebase deploy --only hosting
+
+*/
